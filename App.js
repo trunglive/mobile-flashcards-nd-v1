be6e5eacff5a71ececbef5ecb882a2800b20072b
Trung Vo <trungvo@Trungs-MacBook-Pro.local>
2018-03-09 23:08:53 -0500

    refactor(App): convert to stateless functional component

--- App.js
+++ App.js
@@ -1,48 +1,48 @@
-import React, { Component } from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { View, StyleSheet, StatusBar } from 'react-native';
-import { Constants } from 'expo';
-import { StackNavigator } from 'react-navigation';
-import DeckListView from './components/DeckListView';
-import SingleDeckView from './components/SingleDeckView';
-import NewCardView from './components/NewCardView';
-import QuizView from './components/QuizView';
-import reducer from './reducers';
+import React, { Component } from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { View, StyleSheet, StatusBar } from "react-native";
+import { Constants } from "expo";
+import { StackNavigator } from "react-navigation";
+import DeckListView from "./components/DeckListView";
+import SingleDeckView from "./components/SingleDeckView";
+import NewCardView from "./components/NewCardView";
+import QuizView from "./components/QuizView";
+import reducer from "./reducers";
 
 const Home = StackNavigator({
   DeckListView: { screen: DeckListView },
   SingleDeckView: { screen: SingleDeckView },
   NewCardView: { screen: NewCardView },
   QuizView: { screen: QuizView }
-})
+});
 
-function FlashCardStatusBar ({backgroundColor, ...props}) {
+function FlashCardStatusBar({ backgroundColor, ...props }) {
   return (
     <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
       <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </View>
-  )
+  );
 }
 
-class App extends Component {
-  render() {
-    
-    return (
-      <Provider store={createStore(reducer)}>
-        <View style={styles.container}>
-        <FlashCardStatusBar backgroundColor='#756C83' barStyle="light-content" />
-          <Home />
-        </View>
-      </Provider>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Provider store={createStore(reducer)}>
+      <View style={styles.container}>
+        <FlashCardStatusBar
+          backgroundColor="#756C83"
+          barStyle="light-content"
+        />
+        <Home />
+      </View>
+    </Provider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FBFBFB'
+    backgroundColor: "#FBFBFB"
   }
 });
 
