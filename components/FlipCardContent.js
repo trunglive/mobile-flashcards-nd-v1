e9d1637e03d31cd291168c0c6815591b24eec96c
trunglive <trunglive@gmail.com>
2018-03-26 09:42:28 -0400

    refactor(FlipCard): refactor FlipCardContent component

--- components/FlipCardContent.js
+++ components/FlipCardContent.js
@@ -7,10 +7,10 @@ const FlipCardContent = ({ isFlip, question, answer }) => {
   return (
     <FlipCard style={styles.container} flip={isFlip}>
       <View style={styles.face}>
-        <Text style={styles.question}>{question}</Text>
+        <Text style={styles.cardContent}>{question}</Text>
       </View>
       <View style={styles.back}>
-        <Text style={styles.answer}>{answer}</Text>
+        <Text style={styles.cardContent}>{answer}</Text>
       </View>
     </FlipCard>
   );
@@ -24,16 +24,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center"
   },
-  question: {
-    marginTop: 100,
-    marginLeft: 20,
-    marginRight: 20,
-    marginBottom: 30,
-    textAlign: "center",
-    fontSize: 18,
-    color: primaryColor
-  },
-  answer: {
+  cardContent: {
     marginTop: 100,
     marginLeft: 20,
     marginRight: 20,
